5b02e20a44ab9078f8e942b38b02b0b8db804ce8
Abdurahman bin Ashraf <139567708+Xyntera@users.noreply.github.com>
2025-12-02 08:13:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 window.HOFKI_CONFIG = {
-  // 👇 PASTE YOUR googleusercontent echo URL here (no &type=... added)
-  SCRIPT_BASE: "https://script.googleusercontent.com/macros/echo?user_content_key=XXXXXXX&lib=YYYYYYY",
+  SCRIPT_BASE: "https://script.googleusercontent.com/macros/echo?user_content_key=AAAAAA&lib=BBBBBB",
 
   PRODUCTS_API() {
-    // echo URL already has query params, so we add &type=
     return this.SCRIPT_BASE + "&type=products";
   },
 
@@ -11,7 +9,5 @@ window.HOFKI_CONFIG = {
     return this.SCRIPT_BASE + "&type=order";
   },
 
-  // WhatsApp number (no + sign, include country code)
-  // e.g. +91 7510660614 → "917510660614"
   WHATSAPP_NUMBER: "91XXXXXXXXXX"
 };
