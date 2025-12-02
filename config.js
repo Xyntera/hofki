27646b0e706770db98e8fe0ee9ef1f6960238da9
Abdurahman bin Ashraf <139567708+Xyntera@users.noreply.github.com>
2025-12-02 08:38:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,17 @@
 window.HOFKI_CONFIG = {
-  SCRIPT_BASE: "https://script.google.com/macros/s/AKfycbwh0SJU28gdRTpERDEEg8BCuo8V2X6--lqi3R2EE3Ce-Pd6P9RGfPpID1QZi1ujG9ZJ1g/exec",
+  // Use your googleusercontent echo URL as-is (no extra params)
+  SCRIPT_BASE: "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLh-ioxSNimj6EXqQ8U-peI8iRQ5IaVVMVW9s8EVvhELDMdoyN7MZiQyMqS_kqbH_A2GcnHQ05VonNWUguwRY0EE9hm8W1vUGH-6-vjeU6WNp_YMamc_oXltzZfmbvQYSQHZaaKVKOMyMZB3SSfkUso0DhM9d5ZxHvYaOPlF3fZtgRYDTebVQjacGhcYQmddEI5N1dMvTBNMhCb0S_EfXjSDoexxiDqbjCsJnLgbSsjrMAZ0jiWLmgusrIU7_R6QflaxU8ZL-XHp0q8UnOQI5CLYWNbvtt6i-ZaZ4_QwrnPdyQAbuYI&lib=M6SpIB0tRoB9oJ6-GdKDVxeu0tDxyzix0",
 
   PRODUCTS_API() {
-    return this.SCRIPT_BASE + "?type=products";
+    // GET to echo URL => products JSON
+    return this.SCRIPT_BASE;
   },
 
   ORDER_API() {
-    return this.SCRIPT_BASE + "?type=order";
+    // POST to same echo URL => create order
+    return this.SCRIPT_BASE;
   },
 
+  // Your WhatsApp number (no +, include country code)
   WHATSAPP_NUMBER: "91XXXXXXXXXX"
 };
