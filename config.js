c96b99ad48cd163db5154850d12cf181a1d2cff8
Abdurahman bin Ashraf <139567708+Xyntera@users.noreply.github.com>
2025-12-02 08:16:01 +0530

    Update SCRIPT_BASE URL and API query parameters

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 window.HOFKI_CONFIG = {
-  SCRIPT_BASE: "https://script.googleusercontent.com/macros/echo?user_content_key=AAAAAA&lib=BBBBBB",
+  // use the normal web app URL (no ?type here)
+  SCRIPT_BASE: "https://script.google.com/macros/s/AKfycbwh0SJU28gdRTpERDEEg8BCuo8V2X6--lqi3R2EE3Ce-Pd6P9RGfPpID1QZi1ujG9ZJ1g/exec",
 
   PRODUCTS_API() {
-    return this.SCRIPT_BASE + "&type=products";
+    return this.SCRIPT_BASE + "?type=products";
   },
 
   ORDER_API() {
-    return this.SCRIPT_BASE + "&type=order";
+    return this.SCRIPT_BASE + "?type=order";
   },
 
+  // your WhatsApp number (no +, include country code)
   WHATSAPP_NUMBER: "91XXXXXXXXXX"
 };
